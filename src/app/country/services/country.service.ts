278a27459beb3db4f2c08070440f714f4e45bb17
Jonas Morales <jonasjosuemo2021@outlook.com>
2022-08-05 17:22:54 -0600

    future searching country by region has already done

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -10,12 +10,16 @@ export class CountryService {
 
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
+  get fieldsParams(): any {
+    return new this.fieldsParams().set('fiels', 'name,capital,flag,flags,population,cioc');
+  }
+
   constructor(private http: HttpClient) { }
 
   searchCountry( searchingByCountryName:string): Observable<Country[]> {
 
     const url = `${this.apiUrl}/name/${searchingByCountryName}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {params:this.fieldsParams});
   }
   searchCapitalCity( searchingByCapitalCity:string): Observable<Country[]> {
 
@@ -26,6 +30,11 @@ export class CountryService {
   shownCountry( countryId : string): Observable<Country[]> {
 
     const url = `${this.apiUrl}/alpha/${countryId}`;
+    return this.http.get<Country[]>(url);
+  }
+
+  searchingByRegion( region: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/region/${region}`;
     return this.http.get<Country[]>(url);
   }
 
